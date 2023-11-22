d601f3d63de97e19ccd9edbedb3bd6cc26019cef
Marco-SBG <151732940+lasagraterritorios@users.noreply.github.com>
2023-11-22 16:13:03 +0100

    commit2

--- info_cp/info_cp.js
+++ info_cp/info_cp.js
@@ -26,40 +26,52 @@ const informacionPorCP = {
             { nombre: "Empresa 2", direccion: "Calle 2, Madrid", googleMaps: "https://maps.google.com/?q=Calle2,Madrid" }
         ]
     },
+    "28902": {
+        paginaWeb: "https://www.mcmxiv.com/alba/print-mk?territory=4087,725554,0,d4b9e33c&&nv&address_only=0&m=1&o=1&l=1&d=1&c_n=1&c_t=1&c_l=1&c_nt=1&g=0&cl=1&clm=20&clss=1&st=1,2,3",
+        empresas: []
+    },
     // Agrega información para otros códigos postales según sea necesario
 };
 
 function mostrarInformacionPorCP() {
     const infoSection = document.getElementById("infoSection");
-    const empresasSection = document.getElementById("empresasSection");
+    const empresasSection = document.getElementById("tablaDeEmpresas");
 
     if (informacionPorCP.hasOwnProperty(codigoPostal)) {
         console.log(true)
         const infoCP = informacionPorCP[codigoPostal];
 
         const enlace = `
-            <p>Visita la página web: <a href="${infoCP.paginaWeb}" target="_blank">Aqui</a></p>`;
+            <p>Accede a traves de Alba <a href="${infoCP.paginaWeb}" target="_blank">Aqui</a></p>
+            <p>Accede a través de Google Maps <a href="https://maps.google.com/?q=${codigoPostal}" target="_blank">Aqui</a></p> `;
 
         console.log(enlace);
-        const empresas = `
-            <ul>
+        infoSection.innerHTML = enlace;
+        if (infoCP.empresas && infoCP.empresas.length > 0) {
+            const empresas = `
                 ${infoCP.empresas.map(empresa => `
-                    <li>
-                        <strong>${empresa.nombre}</strong><br>
-                        Dirección: ${empresa.direccion}<br>
-                        Enlace a Google Maps: <a href="${empresa.googleMaps}" target="_blank">Ver en Google Maps</a>
-                    </li>
+                <tr>
+                            <td>${empresa.nombre}</td>
+                            <td>${empresa.direccion}</td>
+                            <td><a href="${empresa.googleMaps}" target="_blank">Mapa</a></p></td>
+                        </tr>
                 `).join('')}
-            </ul>
         `;
 
-        console.log(empresas);
-        infoSection.innerHTML = enlace;
-        empresasSection.innerHTML = empresas
+        empresasSection.innerHTML = empresas;
+
+        } else {
+            console.log("vacio")
+            empresasSection.closest(".col-md-6").style.display = "none";
+            empresasSection.closest("section").querySelector("#empresasSection").innerText = "No hay empresas asignadas a este territorio";
+
+        }       
+       
     } else {
         console.log(false)
     }
 }
+
 
 window.onload = function() {
     console.log("HOla")
