dde864839c72ce46e5a63e68ac3448bcfbe6e59b
Marco-SBG <151732940+lasagraterritorios@users.noreply.github.com>
2023-12-05 15:24:27 +0100

    Update info_cp.js

--- info_cp.js
+++ info_cp.js
@@ -17,59 +17,61 @@ const codigoPostal = obtenerCodigoPostalDeURL();
 document.title = "Info de " + codigoPostal
 document.getElementById("codigoPostal").innerText = codigoPostal;
 
-var informacionPorCP;
-
-fetch('territories.json')
-    .then(response => response.json())
-    .then(data => {
-        // Usa la información cargada desde el archivo JSON
-        informacionPorCP = data;
-
-        // Asegúrate de actualizar el contenido de la página después de cambiar el idioma
-        // Puedes llamar a la función translatePage aquí si es necesario
-    });
 
 
 function mostrarInformacionPorCP() {
-    const infoSection = document.getElementById("infoSection");
-    const empresasSection = document.getElementById("tablaDeEmpresas");
+    fetch('territories.json')
+        .then(response => response.json())
+        .then(data => {
+                // Usa la información cargada desde el archivo JSON
+                const informacionPorCP = data;
+                const infoSection = document.getElementById("infoSection");
+                const empresasSection = document.getElementById("tablaDeEmpresas");
 
-    if (informacionPorCP.hasOwnProperty(codigoPostal)) {
-        console.log(true)
-        const infoCP = informacionPorCP[codigoPostal];
+                if (informacionPorCP.hasOwnProperty(codigoPostal)) {
+                    console.log(true)
+                    const infoCP = informacionPorCP[codigoPostal];
 
-        const enlace = `
-            <iframe src="${infoCP.paginaWeb}" frameborder="0" width="100%" height="400px"></iframe>
-            <div class="territory-buttons">
-            <a href="${infoCP.paginaWeb}" target="_blank"><i class="fa-regular fa-eye"></i><span data-translate-key="territory_alba_link"></span></a>
-            <a href="https://maps.google.com/?q=${codigoPostal}" target="_blank"><i class="fa-solid fa-diamond-turn-right"></i> <span data-translate-key="territory_googlemaps_link"></span></a>
-            </div> `;
+                    const enlace = `
+                <iframe src="${infoCP.paginaWeb}" frameborder="0" width="100%" height="400px"></iframe>
+                <div class="territory-buttons">
+                <a href="${infoCP.paginaWeb}" target="_blank"><i class="fa-regular fa-eye"></i><span data-translate-key="territory_alba_link"></span></a>
+                <a href="https://maps.google.com/?q=${codigoPostal}" target="_blank"><i class="fa-solid fa-diamond-turn-right"></i> <span data-translate-key="territory_googlemaps_link"></span></a>
+                </div> `;
 
-        console.log(enlace);
-        infoSection.innerHTML = enlace;
-        if (infoCP.empresas && infoCP.empresas.length > 0) {
-            const empresas = `
-                ${infoCP.empresas.map(empresa => `
-                <tr>
-                            <td>${empresa.nombre}</td>
-                            <td>${empresa.direccion}</td>
-                            <td><a href="${empresa.googleMaps}" target="_blank"><i class="fa-solid fa-map-location" style="color:black;"></i></a></td>
-                        </tr>
-                `).join('')}
-        `;
-
-        empresasSection.innerHTML = empresas;
-
+                    console.log(enlace);
+                    infoSection.innerHTML = enlace;
+                    if (infoCP.empresas && infoCP.empresas.length > 0) {
+                        const empresas = `
+                    ${infoCP.empresas.map(empresa => `
+                    <tr>
+                                <td>${empresa.nombre}</td>
+                                <td>${empresa.direccion}</td>
+                                <td><a href="${empresa.googleMaps}" target="_blank"><i class="fa-solid fa-map-location" style="color:black;"></i></a></td>
+                            </tr>
+                    `).join('')}
+            `;
+    
+            empresasSection.innerHTML = empresas;
+    
+            } else {
+                console.log("vacio")
+                empresasSection.closest(".col-md-6").style.display = "none";
+                empresasSection.closest("section").querySelector("#empresasSection").innerText = "No hay empresas asignadas a este territorio";
+    
+            }       
+           
         } else {
-            console.log("vacio")
-            empresasSection.closest(".col-md-6").style.display = "none";
-            empresasSection.closest("section").querySelector("#empresasSection").innerText = "No hay empresas asignadas a este territorio";
+            console.log(false)
+        }
+    });
+}
 
-        }       
-       
-    } else {
-        console.log(false)
-    }
+
+
+
+function mostrarInformacionPorCP() {
+    
 }
 
 
